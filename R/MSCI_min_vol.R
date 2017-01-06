ea4046ab4dab0632f3a11fcb91e7272909cb0b9b
johngilheany <johngilheany@gmail.com>
2017-01-06 11:51:46 -0500

    efwfr

--- R/MSCI_min_vol.R
+++ R/MSCI_min_vol.R
@@ -1,3 +1,4 @@
+#test
 getPath_min_vol <- function(){
   source_link <- "https://www.ishares.com/us/products/239695/ishares-msci-usa-minimum-volatility-etf/1467271812596.ajax?fileType=csv&fileName=USMV_holdings&dataType=fund"
   url <- getwd()
